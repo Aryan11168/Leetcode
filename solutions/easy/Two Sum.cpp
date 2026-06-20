594661ce35821806923f0bebf519b0763d7f4c8d
Aryan11168 <chilkewararyan26@gmail.com>
2026-06-21 03:36:55 +0530

    Add solution for Two Sum

---
+++ solutions/easy/Two Sum.cpp
@@ -0,0 +1,11 @@
+// Title: Two Sum
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
