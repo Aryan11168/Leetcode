3880f4e0b12922d2fc49710fe80affd0e71f41fe
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-06-28 15:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.06%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int cnt=0;
-        queue<pair<int,int>> q;
-        vector<vector<int>> dirs={{0,1},{0,-1},{1,0},{-1,0}};
-        int m=grid.size();
-        int n=grid[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2) q.push({i,j});
-            }
+
+int orangesRotting(vector<vector<int>>& grid) {
+    int cnt=0;
+    queue<pair<int,int>> q;
+    vector<vector<int>> dirs={{0,1},{0,-1},{1,0},{-1,0}};
+    int m=grid.size();
+    int n=grid[0].size();
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==2) q.push({i,j});
         }
-        if(q.empty()) cnt=1;
-        while(!q.empty()){
-            int s=q.size();
-            cnt++;
-            while(s--){
-                auto [x,y]=q.front();q.pop();
-                for(auto v:dirs){
-                    int a=x+v[0];
-                    int b=y+v[1];
-                    if(a>=0 && a<m && b>=0 && b<n && grid[a][b]==1){
-                        grid[a][b]=2;
-                        q.push({a,b});
-                    }
+    }
+    while(!q.empty()){
+        int s=q.size();
+        cnt++;
+        while(s--){
+            auto [x,y]=q.front();q.pop();
+            for(auto v:dirs){
+                int a=x+v[0];
+                int b=y+v[1];
+                if(a>=0 && a<m && b>=0 && b<n && grid[a][b]==1){
+                    grid[a][b]=2;
+                    q.push({a,b});
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) return -1;
-            }
-        }
-        return cnt-1;
-
     }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1) return -1;
+        }
+    }
+    return cnt==0?0:cnt-1;
+}
+
 };
