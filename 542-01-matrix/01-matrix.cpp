2a78164d12da3e30c4283b88cc9f20f7a1e566fe
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-06-28 16:26:36 +0530

    Time: 182 ms (5.03%) | Memory: 75 MB (5.16%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,12 +4,12 @@ public:
         int m=mat.size();
         int n=mat[0].size();
         queue<pair<int,int>> q;
-        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<vector<int>> dist(m,vector<int>(n,-1));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
+                    dist[i][j]=0;
                     q.push({i,j});
-                    vis[i][j]=1;
                 }
             }
         }
@@ -19,13 +19,12 @@ public:
             for(auto v:dirs){
                 int a=x+v[0];
                 int b=y+v[1];
-                if(a>=0 && a<m && b>=0 && b<n && !vis[a][b]){
-                    vis[a][b]=1;
-                    mat[a][b]=mat[x][y]+1;
+                if(a>=0 && a<m && b>=0 && b<n && dist[a][b]==-1){
+                    dist[a][b]=dist[x][y]+1;
                     q.push({a,b});
                 }
             }
         }
-        return mat;
+        return dist;
     }
 };
