368c83066ae5227fd75bce49dde9288931dd8045
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-07-02 00:06:10 +0530

    Time: 1348 ms (6.98%) | Memory: 393.3 MB (6.57%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n=grid.size();
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return 0;
+        vector<vector<int>> safe(n,vector<int>(n,-1));
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0) continue;
+                q.push({i,j});
+                safe[i][j]=0;
+            }
+        }
+        vector<vector<int>> dirs={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                auto [x,y]=q.front();q.pop();
+                for(auto v:dirs){
+                    int a=x+v[0];
+                    int b=y+v[1];
+                    if(min(a,b)>=0 && max(a,b)<n && safe[a][b]==-1){
+                        safe[a][b]=safe[x][y]+1;
+                        q.push({a,b});
+                    }
+                }
+            }
+        }
+        priority_queue<tuple<int,int,int>> pq;
+        pq.push({safe[0][0],0,0});
+        safe[0][0]=-1;
+        while(!pq.empty()){
+            auto [dist,x,y]=pq.top();pq.pop();
+            if(x==n-1 && y==n-1) return dist;
+            for(auto v:dirs){
+                int a=x+v[0];
+                int b=y+v[1];
+                if(min(a,b)>=0 && max(a,b)<n && safe[a][b]!=-1){
+                    pq.push({min(dist,safe[a][b]),a,b});
+                    safe[a][b]=-1;
+                }
+            }
+        }
+        return 0;
+
+    }
+};
