f200f1fe9d42e4a2ccc92b1237a7baaac63801ed
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-07-10 01:30:41 +0530

    Time: 0 ms (100.00%) | Memory: 140.6 MB (25.00%) - LeetSync

--- 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -8,9 +8,24 @@ public:
         ans+=abs(lsum-rsum);
         return cost[node]+max(lsum,rsum);
     }
-    int minIncrements(int n, vector<int>& cost) {
+    int minIncrements2(int n, vector<int>& cost) {
         ans=0;
         help(0,n,cost);
         return ans;
+    }
+    // there is a optimal approach
+    int minIncrements(int n, vector<int>& cost) {
+        int cnt = 0;
+
+        for (int i = n / 2; i > 0; i--) {
+            int left = i * 2;
+            int right = i * 2 + 1;
+          
+            cnt += abs(cost[left - 1] - cost[right - 1]);
+
+            cost[i - 1] += max(cost[left- 1], cost[right- 1]);
+        }
+      
+        return cnt;
     }
 };
