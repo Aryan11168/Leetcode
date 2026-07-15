7b2bdc1132bb6113ee198aca450167b340dfb279
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-07-15 13:33:54 +0530

    Time: 22 ms (88.71%) | Memory: 92.1 MB (29.25%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,26 +1,55 @@
 class StockSpanner {
 public:
-    vector<int> nums;
-    StockSpanner() { // constructor - value initialise
-        nums.clear();
-    }
-    // 60,70,60,80,100,70,75
-    int find(){
-        int cnt=0;
-        int n=nums.size();
-        int i=n-1;
-        while(i>=0){
-            if(nums[i]<=nums[n-1]) cnt++;
-            else break;
-            i--;
-        }
-        return cnt;
+    stack<pair<int, int>> stk;
+    int day;
+    StockSpanner() {
+        day = 0;
     }
     int next(int price) {
-        nums.push_back(price);
-        return find();
+        int res = 1;
+        while (!stk.empty() && stk.top().first <= price) {
+            stk.pop();
+        }
+        if (!stk.empty()) {
+            res =  day - stk.top().second;
+        }
+        else {
+            res = day + 1; 
+        }
+        stk.push({price, day++});
+        return res;
     }
 };
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+// class StockSpanner {
+// public:
+//     vector<int> nums;
+//     StockSpanner() { // constructor - value initialise
+//         nums.clear();
+//     }
+//     // 60,70,60,80,100,70,75
+//     int find(){
+//         int cnt=0;
+//         int n=nums.size();
+//         int i=n-1;
+//         while(i>=0){
+//             if(nums[i]<=nums[n-1]) cnt++;
+//             else break;
+//             i--;
+//         }
+//         return cnt;
+//     }
+//     int next(int price) {
+//         nums.push_back(price);
+//         return find();
+//     }
+// };
 // class StockSpanner {
 // public:
 //     vector<int> nums;
@@ -30,27 +59,28 @@ public:
 //     int next(int price) {
 //         nums.push_back(price);
 //         // idhar code likh
-//         int n= //size of given arr of stocks
+//         vector<int> arr=nums;
+//         int n=arr.size();
 //       vector<int>v;
 //       stack<pair<int,int>>st;
 //       //to find previous greater element
 //     for(int i=0; i<n; i++){  //tarversing left to right
 //        if(st.size()==0) v.push_back(-1);
 //        //if non empty and greater milgya
-//        else if(st.size!=0 && st.top()>arr[i]) v.push_back(st.top.first());
+//        else if(st.size()>0 && st.top().first>arr[i]) v.push_back(st.top().second);
 //        //if non empty but greater nhi mila
-//        else if(st.size()!=0; &&  st.top()<=arr[i]){
+//        else if(st.size()>0 &&  st.top().first<=arr[i]){
 //           //pop karte jao
-//           while(!st.epmty() && st.top()<=arr[i]) st.pop();
+//           while(!st.empty() && st.top().first<=arr[i]) st.pop();
 //           if(st.empty()) v.push_back(-1);  //agar khali hogya
-//           else v.push_back(st.top.first());  //greater milgya
+//           else v.push_back(st.top().second);  //greater milgya
 //        }
 //        st.push({arr[i],i});
 //     }
 //     for(int i=0; i<n; i++){
 //         v[i]=i-v[i];
 //     }
-//     return v;
+//     return v.back();
 //     }
 // };
 
