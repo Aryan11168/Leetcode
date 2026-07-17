677e015494f6edacb103624272dd3a20277f6a33
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-07-17 19:40:53 +0530

    Time: 40 ms (76.00%) | Memory: 130.2 MB (44.00%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -13,27 +13,22 @@ public:
         for (int gcdValue = maxValue; gcdValue >= 1; gcdValue--) {
             long long total = 0;
 
-            for (int multiple = gcdValue; multiple <= maxValue; multiple += gcdValue) {
+            for (int multiple = gcdValue; multiple <= maxValue;
+                 multiple += gcdValue) {
                 total += freq[multiple];
             }
 
             long long pairs = total * (total - 1) / 2;
 
-            for (
-                int multiple = 2 * gcdValue;
-                multiple <= maxValue;
-                multiple += gcdValue
-            ) {
+            for (int multiple = 2 * gcdValue; multiple <= maxValue;
+                 multiple += gcdValue) {
                 pairs -= count[multiple];
             }
-
             count[gcdValue] = pairs;
         }
-
         vector<long long> prefix;
         vector<int> values;
         long long sum = 0;
-
         for (int gcdValue = 1; gcdValue <= maxValue; gcdValue++) {
             if (count[gcdValue] > 0) {
                 sum += count[gcdValue];
@@ -41,19 +36,12 @@ public:
                 values.push_back(gcdValue);
             }
         }
-
         vector<int> result;
-
         for (long long query : queries) {
-            int index = lower_bound(
-                prefix.begin(),
-                prefix.end(),
-                query + 1
-            ) - prefix.begin();
-
+            int index = lower_bound(prefix.begin(), prefix.end(), query + 1) -
+                        prefix.begin();
             result.push_back(values[index]);
         }
-
         return result;
     }
 };
