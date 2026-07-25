cf391b85880b968659d4cf1568e89e8e73e474f7
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-07-25 08:25:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (54.99%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     int maxProduct(int n) {
-       string s=to_string(n);
-       sort(s.begin(),s.end());
-       int a=s.length();
-       return (s[a-1]-'0') * (s[a-2]-'0');
+       vector<int> mark(10,0);
+       while(n>0){
+        mark[n%10]++;
+        n/=10;
+       }
+       int ans=1;
+       int cnt=0;
+       for(int i=9;i>=0;i--){
+        if(cnt==2) return ans;
+        if(mark[i]>0){
+            ans*=i;
+            mark[i]--;
+            cnt++;
+        }
+        if(cnt<2 && mark[i]>0){
+            ans*=i;
+            mark[i]--;
+            cnt++;
+        }
+       }
+        return 0;
     }
 };
