89468fd0b4c25c1ff77b048fa17524c9110ba44b
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-08-01 16:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (52.91%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int help(int l,int r,vector<int>& nums){
+    int help(int l,int r,vector<int>& nums,vector<vector<int>>& dp){
         if(l==r) return nums[l];
-        int right=nums[r]-help(l,r-1,nums);
-        int left=nums[l]-help(l+1,r,nums);
-        return max(left,right);
+        if(dp[l][r]!=INT_MIN) return dp[l][r];
+        int right=nums[r]-help(l,r-1,nums,dp);
+        int left=nums[l]-help(l+1,r,nums,dp);
+        return dp[l][r]=max(left,right);
     }
     bool predictTheWinner(vector<int>& nums) {
-        int r=nums.size()-1;
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
+        int r=n-1;
         int l=0;
-        return help(l,r,nums)>=0;
+        return help(l,r,nums,dp)>=0;
     }
 };
