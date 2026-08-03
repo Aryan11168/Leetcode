1ed87a1ec6dbf83686da36e8e830ebd4ef96b52d
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-08-03 23:41:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.57%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -6,31 +6,40 @@ public:
     // b
     // c
     // 3 => d,e,f;
-    // a
-    // b
-    // c
-    
-    void f(int ind,string &digits,vector<string>&alpha,vector<string>&ans,string curr){
-        if(digits.empty()) return;
-        if(ind== digits.size())
-        {
+    // a d
+    // b d
+    // c d
+    // a e
+    // b e
+    // c e
+    // a f
+    // b f
+    // c f
+    void help(int ind,string& curr,string& digits,vector<string>& alpha,vector<string>& ans){
+        if(ind==digits.length()){
             ans.push_back(curr);
             return;
         }
-         string letters= alpha[digits[ind]-'0'];
-
-        for(char ch: letters){
-            curr.push_back(ch);              // choose
-            f(ind+1, digits, alpha,ans, curr); // explore
-            curr.pop_back();                 // backtrack
+        // string a=alpha[ind];
+        // ind=0 digits="23" 
+        string a=alpha[digits[ind]-'0'];
+        for(char c:a){
+            curr.push_back(c);
+            // help(1,"a",)
+            help(ind+1,curr,digits,alpha,ans);
+            // curr="a"
+            curr.pop_back();
+            // curr=""
         }
+        return;
     }
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
+        if(digits.length()==0) return ans;
         vector<string>alpha={
-            "", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
+            "", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         string curr="";
-        f(0,digits, alpha,ans, curr);
+        help(0,curr,digits,alpha,ans);
         return ans;
     }
 };
