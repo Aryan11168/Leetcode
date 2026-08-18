460f60d9777bcc7cc9069e5f77f0a6981d65917d
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-08-18 09:30:42 +0530

    Time: 2122 ms (8.84%) | Memory: 78.5 MB (21.39%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> ans(n,0);
+        for(auto v:bookings){
+            int s=v[0];
+            int e=v[1];
+            int add=v[2];
+            for(int i=s;i<=e;i++){
+                ans[i-1]+=add;
+            }
+        }
+        return ans;
+    }
+};
