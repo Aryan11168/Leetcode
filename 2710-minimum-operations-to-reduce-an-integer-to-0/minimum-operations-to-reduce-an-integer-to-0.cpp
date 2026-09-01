e922abd82893170219357367b1a3d7b4125e1323
Aryan Chilkewar <chilkewararyan26@gmail.com>
2026-09-01 21:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (55.22%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int minOperations(int n) {
-        string s= bitset<32>(n).to_string();
-        int count=0;
+        string s=bitset<32>(n).to_string();
+        int cnt=0;
         int op=0;
-        for(int i= 31; i>=0; i--){
-           if(s[i]=='1')count++;
-           else{
-             if(count==0) continue;
-             else if(count==1) {
-                op+=1;
-                count=0;
-             }
-             else{
-                op++;
-                count=1;
-             }
-           }
-        }
-        if(count!=0){
-            if(count==1) op++;
-            else{
-                op+=2;
+        for(int i=31;i>=0;i--){
+            if(s[i]=='1') cnt++;
+            else {
+                if(cnt==1){
+                    op++;
+                    cnt=0;
+                }
+                else if(cnt==0){
+                    continue;
+                }
+                else{
+                    op++;
+                    cnt=1;
+                }
             }
+        }
+        if(cnt==1) op++;
+        else if(cnt>=2){
+            op+=2;
         }
         return op;
     }
